190b4d9c7c12d13dde9cfa18ac43a4818226014d
Kate Semenova <e.semenova@ktsstudio.ru>
2019-10-20 11:42:35 +0300

    select

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ function bubble(arr) {
 // console.log(bubble([5, 1, 4, 2, 8]));
 // console.log(bubble([8, 1, 4, 2, 5]));
 // console.log(bubble([8, 1, 4, 10, 5]));
+
+function select(arr) {
+    var sorted = [];
+    while (arr.length > 0) {
+        var min = arr[0];
+        var minIndex = 0;
+        arr.forEach(function (item, i) {
+            if (item < min) {
+                min = item;
+                minIndex = i;
+            }
+        });
+        arr.splice(minIndex, 1);
+        sorted.push(min);
+    }
+    return sorted;
+}
+
+// console.log(select([2, 1]));
+// console.log(select([2, 1, 3]));
+// console.log(select([2, 3, 1]));
+// console.log(select([0, 2, 1, 4]));
